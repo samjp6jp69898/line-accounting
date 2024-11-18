5d6dd438fe49ab8d0210e864eb25eb0e5294754e
Landon <pkh_samjp6jp69898@photons.com.tw>
2024-11-18 19:09:04 +0800

    fix: id & secret

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ const config = {
   lineCheckFreindShipApiUrl: "https://api.line.me/friendship/v1/status",
   lineAddFriendApiUrl: "https://line.me/R",
   // redirectUri: process.env.VUE_APP_REDIRECT_URI,
-  clientId: "4f03d9065b14f7d6e394a0f20b0c53e3",
-  channelSecret: "1661441879",
+  clientId: "1661441879",
+  channelSecret: "4f03d9065b14f7d6e394a0f20b0c53e3",
   lineId: "@203bprvs"
 };
 
